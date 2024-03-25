3e9cfe3f31df545bfe4ac6e7076869db87e7a165
Nikola971 <mukola.milyan@outlook.com>
2024-03-25 12:05:23 +0200

    936

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// index.js
+
+import { program } from "commander";
+import { listContacts, getContactById, removeContact, addContact } from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "contact id")
+  .option("-n, --name <type>", "contact name")
+  .option("-e, --email <type>", "contact email")
+  .option("-p, --phone <type>", "contact phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.table(await listContacts());
+      break;
+
+    case "get":
+      console.log(await getContactById(id));
+      break;
+
+    case "add":
+      console.log(await addContact(name, email, phone));
+      break;
+
+    case "remove":
+      console.log(await removeContact(id));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
